8788e2feef0d5899329488c4ecafae4a170cd63e
ncou <nicolas_courade@yahoo.fr>
2020-06-21 13:35:50 +0200

    update

--- config/paths.php
+++ config/paths.php
@@ -4,7 +4,7 @@ $rootPath = dirname(__DIR__, 1);
 
 return [
     '@root'         => $rootPath,
-    '@app'          => '@root/app',
+    '@app'          => '@root/src',
     '@config'       => '@root/config',
     '@public'       => '@root/public',
     '@resources'    => '@root/resources',
