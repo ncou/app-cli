001a59f89ca5972fc7be1a2f371fd105125d6c5c
ncou <nicolas_courade@yahoo.fr>
2021-03-13 15:57:23 +0100

    update

--- cli.php
+++ cli.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 $currentDir = PHP_SAPI === 'cli' ? getcwd() : realpath(getcwd().'/../');
 
-require $currentDir . '/bootstrap/requirements.php';
 require $currentDir . '/vendor/autoload.php';
 
 Chiron\Debug\Debug::enable();
