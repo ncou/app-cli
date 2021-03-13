322ab14e8e1670ebb272e8aac030db7bdc94924f
ncou <nicolas_courade@yahoo.fr>
2021-03-13 15:58:44 +0100

    update

--- app.php
+++ app.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
+Chiron\Debug\Debug::enable();
+
 $paths = require __DIR__ . '/bootstrap/paths.php';
 $app = Chiron\Application::init($paths);
 
